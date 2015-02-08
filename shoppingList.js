ce1ec5ae89b7871dc809d136ea4519a9eb3b6cdb
fangminglu <xiaofangwuyu@outlook.com>
2015-02-08 22:48:17 +0800

    test git commit

---
+++ shoppingList.js
@@ -0,0 +1,34 @@
+function showClientMain()
+{
+    var allItemsStr=localStorage.getItem('allItemsDb');
+    var allItems=JSON.parse(allItemsStr);
+    $(document).ready(function()
+    {
+        bindGoodsKey(allItems);
+        addCartNum();
+    });
+}
+function bindGoodsKey(allItems)
+{
+    var btnInsertCart='<button>加入购物车</button>';
+    $.each(allItems,function(i)
+    {
+        var insertRow="<tr>"+
+            "<td>"+allItems[i].sort+"</td>"+
+            "<td>"+allItems[i].name+"</td>"+
+            "<td>"+allItems[i].unit+"</td>"+
+            "<td>"+allItems[i].price+"</td>"+
+            "<td>"+btnInsertCart+"</td>"+
+            "</tr>";
+        $('#itemListTab').append(insertRow);
+    });
+}
+function addCartNum()
+{
+    $('#itemListTab').find('button').on('click',function()
+    {
+        var name=$(this).closest('tr').find('td').eq(1).text();
+        addNum(name);
+        refreshCart();
+    })
+}
